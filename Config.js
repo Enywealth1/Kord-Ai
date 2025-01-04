f27432ab312f63a149c0ec70bedbe59fddf2297b
Enywealth1 <liamfa68@gmail.com>
2025-01-04 17:03:52 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
+  OWNER_NUMBERS: "2349023074606", // your number with country code
+  OWNER_NAME: 'Enywealth', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: '', // put your session id between the ''
+  SESSION_ID: 'kord_ai-Da6EznuR64GG', // put your session id between the ''
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
